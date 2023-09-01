0156482f3c7df05a1016d8316780eb41b9792315
Metaverseforpc <57561743+vaibhavard@users.noreply.github.com>
2023-09-01 16:09:00 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import cron from 'node-cron';
+import fetch from 'node-fetch';
 
-cron.schedule(`*/1 * * * *`, async () => {
-  console.log(`running your task...`);
+const url = 'https://intagpt.onrender.com/';
+
+cron.schedule('*/30 * * * *', async () => {
+  try {
+    console.log('Sending GET request...');
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.text();
+      console.log('GET request successful:', data);
+    } else {
+      console.error('GET request failed:', response.status, response.statusText);
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
 });
